631752033590d65c9a4783d03e7db467be2644e4
upskillbootcamps00123 <upskillbootcamps@gmail.com git config --global user.email upskillbootcamps@gmail.com git config --global user.email upskillbootcamps@gmail.com>
2025-02-26 14:33:51 +0430

    we have been create some new files to push to github

---
+++ another.js
@@ -0,0 +1,5 @@
+import { messages as newmessage, newNamess } from "./index.mjs";
+
+console.log(newmessage);
+
+newNamess("upskill");
---
+++ index.mjs
@@ -0,0 +1,7 @@
+let messages = "this is export var";
+
+function name(username) {
+  console.log("this body of username fun");
+}
+
+export { messages, name as newNamess };
